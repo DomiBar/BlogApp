4d197826f94ed98331517bddd3c62d589699fbfa
Dominik Baran <dominik.baran1992@gmail.com>
2020-08-24 13:00:17 +0200

    Add database

--- blog/__init__.py
+++ blog/__init__.py
@@ -9,3 +9,12 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from blog import routes, models
+from blog.models import Entry, db
+
+
+@app.shell_context_processor
+def make_shell_context():
+  return {
+      "db": db,
+      "Entry": models.Entry
+  }
